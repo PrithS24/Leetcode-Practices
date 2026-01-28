1c77c5b618ca032018a37b263b6b444da77f314e
Pritha Saha <149777871+PrithS24@users.noreply.github.com>
2026-01-29 00:09:27 +0600

    Time: 0 ms (100%), Space: 23.9 MB (19.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& v) {
-        vector<int> p;
-        int s = 0;
-        int f = 0;
-        for (auto i : v) {
-            if (i != 0) {
-                p.push_back(i);
-            } else {
-                s++;
-                f = 1;
+    void moveZeroes(vector<int>& nums) {
+        int z=0;
+      
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[z++]=nums[i];
             }
+            
+            
         }
-        if (f) {
-            for (int i = 0; i < s; i++) {
-                p.push_back(0);
-            }
+        while(z<nums.size())
+        {
+            nums[z++]=0;
         }
-
-        v = p;
+        
     }
 };
